f7bd1a616fa9e1cc72ca5b17528b9ea8da5cfb5a
Cristian Oliveira <contato@cristianoliveira.com.br>
2017-05-31 23:30:12 +0200

    feature: responds with errors as json

--- api/api_test.go
+++ api/api_test.go
@@ -1,117 +1,157 @@
 package api
 
 import (
-  "fmt"
-  "os"
-  "io"
-  "strings"
-  "io/ioutil"
-  "testing"
-  "net/http"
-  "net/http/httptest"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/cristianoliveira/apitogo/api/json"
+	"github.com/cristianoliveira/apitogo/common"
 )
 
 func TestWhenHasCollectionFile(t *testing.T) {
-  err := copyFile("../posts.json", "./posts.json")
-  if err != nil { t.Error(err) }
+	err := copyFile("../posts.json", "./posts.json")
+	if err != nil {
+		t.Error(err)
+	}
 
-  router := Router()
-  ts := httptest.NewServer(router)
-  defer ts.Close()
+	router := Router()
+	ts := httptest.NewServer(router)
+	defer ts.Close()
 
-  resp, err := http.Get(ts.URL + "/posts")
-  if err != nil { t.Error(err) }
+	resp, err := http.Get(ts.URL + "/posts")
+	if err != nil {
+		t.Error(err)
+	}
 
-  t.Run("Responds with OK status", func(t *testing.T) {
-    expected := http.StatusOK
-    result := resp.StatusCode
+	t.Run("Responds with OK status", func(t *testing.T) {
+		expected := http.StatusOK
+		result := resp.StatusCode
 
-    if expected != result {
-      t.Errorf("Expected %s got %s", expected, result)
-    }
-  })
+		if expected != result {
+			t.Errorf("Expected %s got %s", expected, result)
+		}
+	})
 
-  t.Run("Provide collection data", func(t *testing.T) {
+	t.Run("Provide collection data", func(t *testing.T) {
 
-    body, err := ioutil.ReadAll(resp.Body)
-    if err != nil { t.Error(err) }
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			t.Error(err)
+		}
 
-    file, err := ioutil.ReadFile("./posts.json")
-    if err != nil { t.Error(err) }
+		file, err := ioutil.ReadFile("./posts.json")
+		if err != nil {
+			t.Error(err)
+		}
 
-    result := strings.TrimSpace(string(body))
-    expected := strings.TrimSpace(string(file))
+		result := strings.TrimSpace(string(body))
+		expected := strings.TrimSpace(string(file))
 
-    if expected == result {
-      t.Errorf("Expected %s got %s", expected, result)
-    }
-  })
+		if expected == result {
+			t.Errorf("Expected %s got %s", expected, result)
+		}
+	})
 
-  t.Run("Provide json data", func(t *testing.T) {
+	t.Run("Provide json data", func(t *testing.T) {
 
-    resp, err := http.Get(ts.URL + "/posts")
-    if err != nil { t.Error(err) }
+		resp, err := http.Get(ts.URL + "/posts")
+		if err != nil {
+			t.Error(err)
+		}
 
-    result := resp.Header.Get("Content-Type")
-    expected := "application/json; charset=UTF-8"
+		result := resp.Header.Get("Content-Type")
+		expected := "application/json; charset=UTF-8"
 
-    if expected != result {
-      t.Errorf("Expected %s got %s", expected, result)
-    }
-  })
+		if expected != result {
+			t.Errorf("Expected %s got %s", expected, result)
+		}
+	})
 
-  os.Remove("./posts.json")
+	os.Remove("./posts.json")
 }
 
 func TestWhenHasNoCollectionFile(t *testing.T) {
-  router := Router()
-  ts := httptest.NewServer(router)
-  defer ts.Close()
+	router := Router()
+	ts := httptest.NewServer(router)
+	defer ts.Close()
 
-  resp, err := http.Get(ts.URL + "/posts")
-  if err != nil { t.Error(err) }
+	resp, err := http.Get(ts.URL + "/postss")
+	if err != nil {
+		t.Error(err)
+	}
 
-  t.Run("Responds with Bad Request", func(t *testing.T) {
-    expected := http.StatusNotFound
-    result := resp.StatusCode
+	t.Run("Responds with Not Found", func(t *testing.T) {
+		expected := http.StatusNotFound
+		result := resp.StatusCode
 
-    if expected != result {
-      t.Errorf("Expected %s got %s", expected, result)
-    }
-  })
+		if expected != result {
+			t.Errorf("Expected %d got %d", expected, result)
+		}
+	})
 
-  t.Run("Responds with text", func(t *testing.T) {
+	t.Run("Responds with json", func(t *testing.T) {
 
-    result := resp.Header.Get("Content-Type")
-    expected := "text/plain; charset=utf-8"
+		result := resp.Header.Get("Content-Type")
+		expected := "application/json; charset=UTF-8"
 
-    if expected != result {
-      t.Errorf("Expected %s got %s", expected, result)
-    }
-  })
+		if expected != result {
+			t.Errorf("Expected %s got %s", expected, result)
+		}
+	})
 
-  t.Run("Provide error cause", func(t *testing.T) {
-    body, err := ioutil.ReadAll(resp.Body)
-    _, err = ioutil.ReadFile("./posts.json")
+	t.Run("Provide error cause", func(t *testing.T) {
+		body, err := ioutil.ReadAll(resp.Body)
+		_, err = ioutil.ReadFile(common.Settings().PathFile("postss"))
 
-    result := string(body)
-    expected := fmt.Sprintln(err)
+		result := string(body)
+		expected := string(json.NewError(404, err).AsBytes())
 
-    if expected == result {
-      t.Errorf("Expected %s got %s", expected, result)
-    }
-  })
+		if expected != result {
+			t.Errorf("Expected %d got %d", expected, result)
+		}
+	})
+}
+
+func TestUsingWrongParameters(t *testing.T) {
+	router := Router()
+	ts := httptest.NewServer(router)
+	defer ts.Close()
+
+	resp, err := http.Get(ts.URL + "/posts/foo")
+	if err != nil {
+		t.Error(err)
+	}
+
+	t.Run("Responds with Bad Request", func(t *testing.T) {
+		expected := http.StatusBadRequest
+		result := resp.StatusCode
+
+		if expected != result {
+			t.Errorf("Expected %d got %d", expected, result)
+		}
+	})
 }
 
 func copyFile(src, dst string) error {
-  in, err := os.Open(src)
-  if err != nil { return err }
-  defer in.Close()
-  out, err := os.Create(dst)
-  if err != nil { return err }
-  defer out.Close()
-  _, err = io.Copy(out, in)
-  cerr := out.Close()
-  if err != nil { return err }
-  return cerr
+	in, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer in.Close()
+	out, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+	_, err = io.Copy(out, in)
+	cerr := out.Close()
+	if err != nil {
+		return err
+	}
+	return cerr
 }
--- api/json/json.go
+++ api/json/json.go
@@ -1,66 +1,90 @@
 package json
 
 import (
-  "fmt"
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"strconv"
 
+	"github.com/cristianoliveira/apitogo/common"
 	"github.com/gorilla/mux"
-  "github.com/cristianoliveira/apitogo/common"
 )
 
+type Error struct {
+	Code   int      `json:"code"`
+	Errors []string `json:"errors"`
+}
+
+func (e *Error) AsBytes() []byte {
+	data, err := json.Marshal(e)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return data
+}
+
+func NewError(code int, err error) *Error {
+	return &Error{Errors: []string{err.Error()}, Code: code}
+}
+
 func HandleGetAll(w http.ResponseWriter, r *http.Request) {
-  settings := common.Settings()
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	settings := common.Settings()
 	vars := mux.Vars(r)
 
-  path := settings.PathFile(vars["collection"])
+	path := settings.PathFile(vars["collection"])
 	collection, err := CollectionLoad(path)
 	if err != nil {
 		handleNotFound(w, err)
 		return
 	}
 
-  data, err := collection.AsBytes()
+	data, err := collection.AsBytes()
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprintln(w, "Error: ", err)
+		w.Write(NewError(http.StatusBadRequest, err).AsBytes())
 	} else {
-		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 		w.WriteHeader(http.StatusOK)
 		w.Write(data)
 	}
 }
 
 func HandleGetById(w http.ResponseWriter, r *http.Request) {
-  settings := common.Settings()
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	settings := common.Settings()
 	vars := mux.Vars(r)
 
-  path := settings.PathFile(vars["collection"])
+	path := settings.PathFile(vars["collection"])
 	collection, err := CollectionLoad(path)
 
 	pId, err := strconv.ParseFloat(vars["id"], 64)
+	if err != nil {
+		handleBadRequest(w, err)
+		return
+	}
+
+	selected, err := collection.GetById(pId).AsBytes()
 	if err != nil {
 		handleNotFound(w, err)
 		return
 	}
 
-	selected, err := collection.GetById(pId).AsBytes()
-	if selected != nil {
-		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-		w.WriteHeader(http.StatusOK)
-		w.Write(selected)
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Println(w, nil)
-	}
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write(selected)
 }
 
 func handleNotFound(w http.ResponseWriter, err error) {
 	w.WriteHeader(http.StatusNotFound)
-	fmt.Println(w, err)
+	w.Write(NewError(http.StatusNotFound, err).AsBytes())
 }
 
 func handleBadRequest(w http.ResponseWriter, err error) {
-	w.WriteHeader(http.StatusNotFound)
-	fmt.Println(w, err)
+	w.WriteHeader(http.StatusBadRequest)
+	w.Write(NewError(http.StatusBadRequest, err).AsBytes())
+}
+
+func handleServerError(w http.ResponseWriter, err error) {
+	w.WriteHeader(http.StatusInternalServerError)
+	w.Write(NewError(http.StatusInternalServerError, err).AsBytes())
 }
